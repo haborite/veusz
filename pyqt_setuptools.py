4bdd97a3ab9fd6f49bf08a13e7f7c7b007d484fd
haborite <126334158+haborite@users.noreply.github.com>
2024-11-23 12:19:52 +0900

    Update pyqt_setuptools.py

--- pyqt_setuptools.py
+++ pyqt_setuptools.py
@@ -183,8 +183,8 @@ class sip_build_ext(build_ext):
             if self.compiler.compiler_type == 'unix':
                 extension.extra_compile_args.append('-std=c++17')
             elif self.compiler.compiler_type == 'msvc':
-                extension.extra_compile_args.append('/Zc:__cplusplus')
-                extension.extra_compile_args.append('/std:c++17')
+                flags = os.environ.get('QMAKE_CXXFLAGS', '')
+                os.environ['QMAKE_CXXFLAGS'] = flags + ' /std:c++17 /Zc:__cplusplus'
 
         depends = extension.depends
 
