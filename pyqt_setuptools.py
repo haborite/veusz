e889e9fa6035207549d47b7d2c05dd7ae92a7ee5
haborite <126334158+haborite@users.noreply.github.com>
2024-11-24 17:13:14 +0900

    Update pyqt_setuptools.py

--- pyqt_setuptools.py
+++ pyqt_setuptools.py
@@ -250,7 +250,7 @@ class sip_build_ext(build_ext):
 
         toml_text=f'''
 [build-system]
-requires=["sip >= 6.5.1, <7"]
+requires=["sip >= 6.0.0, <7"]
 build-backend="sipbuild.api"
 
 [tool.sip.metadata]
